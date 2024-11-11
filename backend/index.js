c6d54b977c71dd96f54cdf3499c49f9d39a503bc
Wassel <ouasselmeftah@gmail.com>
2024-11-11 01:27:05 +0100

    BACKEND : Defualt port changed.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('CFPAGL SERVER RUNNING!')
-})
+app.get("/", (req, res) => {
+  res.send("CFPAGL SERVER RUNNING!");
+});
 
 app.listen(port, () => {
-  console.log(`CFPAGL SERVER listening on port ${port}`)
-})
+  console.log(`CFPAGL SERVER listening on port ${port}`);
+});
