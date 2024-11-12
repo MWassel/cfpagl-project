342714499b054dc7242ffc53d2fa5c150742b7b6
Wassel <ouasselmeftah@gmail.com>
2024-11-12 23:43:47 +0100

    BACKEND : cors installed and some routes files added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// routes
+const bookRoutes = require("./src/books/book.route");
+app.use("/api/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.send("CFPAGL SERVER RUNNING!");
---
+++ backend/src/books/book.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+// post a book
+router.post("/create-book", async (req, res) => {});
+
+module.exports = router;
